b4b2a2bf6d30d4141810df78b66ee4743fe44eb2
Aleksandr Kornilov <saxa03@mail.ru>
2022-12-13 17:03:01 +0800

    add Task 047 HomeWork

---
+++ Task047_HomeWork/Program.cs
@@ -0,0 +1,46 @@
+﻿//47 заполнение случайными вещественными числами
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int lines =  Convert.ToInt32(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns =  Convert.ToInt32(ReadLine());
+
+double[,] matrix = new double[lines, columns];
+FillArray(matrix);
+PrintArray(matrix);
+
+
+
+
+
+
+// Заполнение массива рандомными вещественными числами
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = Convert.ToDouble (new Random().Next(-50, 50));
+        }
+    }
+}
+
+
+//  Функция вывода двумерного массива в терминал
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Write($"{matr[i, j]} ");
+        }
+        WriteLine();
+    }    
+}
